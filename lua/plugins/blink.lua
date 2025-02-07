d95d287c1cd415655fa08805b34c37d598a9b8be
justlookz <cspios@gmail.com>
2025-02-08 01:11:37 +0200

    fixed snippet jumps

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -11,25 +11,28 @@ return {
                 selection = {
                     preselect = true,
                     auto_insert = false,
-                },
-            },
+                }
+            }
         },
         keymap = {
             ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
             ['<C-e>'] = { 'hide' },
             ['<C-y>'] = { 'select_and_accept', 'show' },
-
             ['<C-n>'] = { 'select_next', 'show', "fallback" },
             ['<C-p>'] = { 'select_prev', 'show', "fallback" },
-
             ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
             ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+            ['<C-j>'] = { 'snippet_forward' },
+            ['<C-k>'] = { 'snippet_backward' },
         },
         sources = {
             default = { 'snippets', 'lsp', 'path', 'buffer', "dadbod" },
             providers = {
-                dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
-            },
-        },
+                dadbod = {
+                    name = "Dadbod",
+                    module = "vim_dadbod_completion.blink",
+                }
+            }
+        }
     }
 }
