4be25dcf2248f8883210a1b6b231cc07f089249a
justlookz <cspios@gmail.com>
2025-02-07 22:52:14 +0200

    move lazy startup to init

--- init.lua
+++ init.lua
@@ -1,3 +1,33 @@
-require("lazy-start")
+vim.g.mapleader = " "
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+--- if path none existed clone lazy to that path
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+--- This is used to load lazy from pligins folder
+require("lazy").setup({
+        { import = "plugins" },
+    },
+
+    {
+        install = {
+            colorscheme = { "nord" },
+        },
+        change_detection = {
+            enabled = true,
+            notify = false,
+        },
+    })
 
 vim.cmd.colorscheme("nord")
