c72e0aa4865ed3e53753d5a1d8aa994e5c30188c
justlookz <cspios@gmail.com>
2025-02-15 15:57:06 +0200

    Fixed lsp attaching to non existant files and performance

--- plugin/autocommand.lua
+++ plugin/autocommand.lua
@@ -147,16 +147,14 @@ aucmd("BufWinLeave", {
     end
 })
 
-local init_vim = augroup("init-vim", { clear = true })
+local init_vim = augroup("kvim-init-vim", { clear = true })
 
 aucmd("VimEnter", {
     group = init_vim,
     pattern = "*",
     callback = function()
         local file_name = vim.fn.expand("%")
-        if file_name ~= "" then
-            vim.cmd.edit(file_name)
-        else
+        if file_name == "" then
             vim.cmd.Explore()
         end
     end
