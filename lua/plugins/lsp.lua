ebf8c755968af4784f1c7533dcb1f121b6eb8415
justlookz <cspios@gmail.com>
2025-02-15 14:18:50 +0200

    Some fixes

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,6 +1,6 @@
 return {
     'neovim/nvim-lspconfig', -- Required
-    opts = {},
+    event = { "BufReadPre", "BufNewFile" },
     dependencies = {
         { 'saghen/blink.cmp' },
         { 'williamboman/mason.nvim' },
@@ -13,80 +13,75 @@ return {
     config = function()
         -- Setup Mason
         require('mason').setup()
-        require('mason-lspconfig').setup({
-            automatic_installation = false })
-
-        -- Setup lsp-config
-        local lsp_config = require("lspconfig")
 
         -- Take capabilities from blink
         local capabilities = require('blink.cmp')
             .get_lsp_capabilities()
 
-        -- Get Mason installed servers
-        local lsp_server = require("mason-lspconfig")
-            .get_installed_servers()
-
         -- Setup languages installed from Mason with
         -- their default settings
-        for _, v in pairs(lsp_server) do
-            if v ~= "jdtls" then
-                lsp_config[v].setup({
-                    capabilities = capabilities })
-            end
-        end
 
-        -------------------------------------
-        -- Setup servers for outside Mason --
-        -- and jdtls with it's extra args  --
-        -------------------------------------
+       local handlers = {
+           -- The first entry (without a key) will be the default handler
+           -- and will be called for each installed server that doesn't have
+           -- a dedicated handler.
+           function (server_name) -- default handler (optional)
+               require("lspconfig")[server_name].setup {
+                    capabilities = capabilities
+               }
+           end,
+           -- Next, you can provide targeted overrides for specific servers.
+           -- Removed default handler for global installed
+           ["jdtls"] = function() end,
+           ["clangd"] = function() end,
+           ["lua_ls"] = function() end,
+           ["tinymist"] = function() end,
+           ["gopls"] = function() end,
+           ["rust_analyzer"] = function() end,
+       }
 
-        local function not_from_mason(check)
-            for _, v in pairs(lsp_server) do
-                if v == check then return false end
-            end
-            return true
-        end
+       -- setup mason lspconfig
+        require('mason-lspconfig').setup({
+            ensure_installed = {},
+            handlers = handlers,
+            automatic_installation = false })
 
-        -- Setup lua language server for Lua
-        if vim.fn.executable("lua-language-server")
-            and not_from_mason("lua_ls") then
-            lsp_config.lua_ls.setup({
+        -- Setup lsp-config
+        local lsp_config = require("lspconfig")
+
+        -- Setup clangd language server for C/C++
+        if vim.fn.executable("clangd") then
+            lsp_config.clangd.setup({
                 capabilities = capabilities })
         end
 
         -- Setup clangd language server for C/C++
-        -- if vim.fn.executable("clangd")
-        --     and not_from_mason("clangd") then
-            lsp_config.clangd.setup({
+        if vim.fn.executable("lua-language-server") then
+            lsp_config.lua_ls.setup({
                 capabilities = capabilities })
-        -- end
+        end
 
         -- Setup gopls for Golang
-        if vim.fn.executable("gopls")
-            and not_from_mason("gopls") then
+        if vim.fn.executable("gopls") then
             lsp_config.gopls.setup({
                 capabilities = capabilities })
         end
 
         -- Setup tinymist for Typst
-        if vim.fn.executable("tinymist")
-            and not_from_mason("tinymist") then
+        if vim.fn.executable("tinymist") then
             lsp_config.tinymist.setup({
                 capabilities = capabilities })
         end
 
 
         -- Setup Rust-analyzer for Rust
-        if vim.fn.executable("rust-analyzer")
-            and not_from_mason("rust_analyzer") then
+        if vim.fn.executable("rust-analyzer") then
             lsp_config.rust_analyzer.setup({
                 capabilities = capabilities })
         end
 
         -- Setup Ruff for Python
-        if vim.fn.executable("ruff")
-            and not_from_mason("ruff") then
+        if vim.fn.executable("ruff") then
             lsp_config.ruff.setup({
                 capabilities = capabilities })
         end
