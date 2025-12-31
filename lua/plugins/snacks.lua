a5fb8079a54ab56baf48617ef097aa97d9030b52
Nick K <cspios@gmail.com>
2025-12-31 16:04:58 +0200

    feat: space picker

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -25,7 +25,7 @@ return {
     },
     keys = {
         -- Top Pickers & Explorer
-        { "<leader><space>", function() Snacks.picker.smart() end,           desc = "Smart Find Files" },
+        { "<leader><space>", function() Snacks.picker.files() end,           desc = "Smart Find Files" },
         { "<leader>,",       function() Snacks.picker.buffers() end,         desc = "Buffers" },
         { "<leader>/",       function() Snacks.picker.grep() end,            desc = "Grep" },
         { "<leader>:",       function() Snacks.picker.command_history() end, desc = "Command History" },
