ee8dfdfc8abbe878b79678f00452fcc34fcbb018
Nick K <cspios@gmail.com>
2026-02-13 01:49:03 +0200

    fix: broken if statemebt

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -24,7 +24,7 @@ local function lsp_setup()
     })
 
     -- Setup clangd language server for C/C++
-    if vim.fn.executable("clangd") then
+    if vim.fn.executable("clangd") == 1 then
         vim.lsp.config("clangd", {
             cmd = { "clangd", "--clang-tidy" },
         })
@@ -32,35 +32,35 @@ local function lsp_setup()
     end
 
     -- Setup lua language server for lua
-    if vim.fn.executable("emmylua_ls") then
+    if vim.fn.executable("emmylua_ls") == 1 then
         vim.lsp.enable("emmylua_ls")
     elseif vim.fn.executable("lua-language-server") then
         vim.lsp.enable('lua_ls')
     end
 
     -- Setup gopls for Golang
-    if vim.fn.executable("gopls") then
+    if vim.fn.executable("gopls") == 1 then
         vim.lsp.enable('gopls')
     end
 
     -- Setup tinymist for Typst
-    if vim.fn.executable("tinymist") then
+    if vim.fn.executable("tinymist") == 1 then
         vim.lsp.enable('tinymist')
     end
 
 
     -- Setup Rust-analyzer for Rust
-    if vim.fn.executable("rust-analyzer") then
+    if vim.fn.executable("rust-analyzer") == 1 then
         vim.lsp.enable("rust_analyzer")
     end
 
     -- Setup Ruff for Python
-    if vim.fn.executable("ruff") then
+    if vim.fn.executable("ruff") == 1 then
         vim.lsp.enable("ruff")
     end
 
     -- Setup ty for Python
-    if vim.fn.executable("ty") then
+    if vim.fn.executable("ty") == 1 then
         vim.lsp.enable("ty")
     end
 end
