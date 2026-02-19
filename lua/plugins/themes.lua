007de0e44821a6ac6a38932b8d1c42ca8d163e80
Nick K <cspios@gmail.com>
2026-02-20 00:22:22 +0200

    italic

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -2,13 +2,6 @@ return {
     {
         "ellisonleao/gruvbox.nvim",
         opts = {
-            italic = {
-                strings = false,
-                emphasis = false,
-                comments = false,
-                operators = false,
-                folds = false,
-            },
         }
     },
 }
