2635419f6e33329139f7b09e4480839bfbb6e6ef
Nick K <cspios@gmail.com>
2026-04-07 14:11:55 +0300

    mkre changes

--- lua/completion.lua
+++ lua/completion.lua
@@ -2,6 +2,6 @@ local o = vim.o
 
 o.pumborder="double"
 o.complete="o"
-o.completeopt="menu,menuone,noselect,fuzzy,popup,nearest,nosort"
+o.completeopt="menu,menuone,fuzzy,popup,nearest,preinsert"
 o.pummaxwidth=30
 o.pumheight=10
